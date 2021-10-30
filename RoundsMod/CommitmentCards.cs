df85ea4f6e1f78c553754baf0794eb83ca88010c
Daniel Klingensmith <dpklinge@ncsu.edu>
2021-10-30 13:11:32 -0400

    Initial template commit

---
+++ RoundsMod/CommitmentCards.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using CommitmentCards.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+
+namespace CommitmentCards
+{
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class CommitmentCards: BaseUnityPlugin
+    {
+        private const string ModId = "com.dk.rounds.CommitmentCards";
+        private const string ModName = "CommitmentCards";
+        public const string Version = "1.0.0"; // (major.minor.patch)?
+        public const string ModInitials = "CC";
+        public static CommitmentCards instance { get; private set; }
+
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            instance = this;
+            CustomCard.BuildCard<MyCardName>();
+        }
+
+    }
+}
