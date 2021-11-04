40f6629d97916d4d239bc9be4de89468c9122e89
pdcook <qphysicscook@gmail.com>
2021-11-04 01:29:40 -0400

    fixed lag from debug logging

--- RoundsMod/Scripts/HandManipulator.cs
+++ RoundsMod/Scripts/HandManipulator.cs
@@ -13,17 +13,17 @@ namespace CommitmentCards.Scripts
         private void Start()
         {
             instance = this;
-            UnityEngine.Debug.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator has been started");
+            CommitmentCards.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator has been started");
         }
         public CardInfo DuplicateRandomCard(Player player, int amount)
         {
-            UnityEngine.Debug.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator DuplicateRandomCard called for player {player} and amount {amount}");
+            CommitmentCards.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator DuplicateRandomCard called for player {player} and amount {amount}");
             var randomCard = SelectRandomCardWithStats(player);
             if (randomCard == null)
             {
                 return null;
             }
-            UnityEngine.Debug.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator DuplicateRandomCard selected card: {randomCard} {randomCard.cardName}");
+            CommitmentCards.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator DuplicateRandomCard selected card: {randomCard} {randomCard.cardName}");
             for (int i=0; i < amount; i++){
                 ModdingUtils.Utils.Cards.instance.AddCardToPlayer(player, randomCard, false, "", 0, 0, true);
             }
@@ -45,31 +45,31 @@ namespace CommitmentCards.Scripts
         }
         public Player RemoveCardType(Player player, CardInfo cardInfo)
         {
-            UnityEngine.Debug.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator RemoveCardType called on card {cardInfo}");
+            CommitmentCards.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator RemoveCardType called on card {cardInfo}");
             ModdingUtils.Utils.Cards.instance.RemoveCardFromPlayer(player, cardInfo, ModdingUtils.Utils.Cards.SelectionType.Random);
-            UnityEngine.Debug.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator RemoveCardType finished for card {cardInfo}");
+            CommitmentCards.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator RemoveCardType finished for card {cardInfo}");
             return player;
         }
         private CardInfo SelectRandomCardWithStats(Player player)
         {
-            UnityEngine.Debug.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator SelectRandomCardWithStats called with player {player}");
-            player.data.currentCards.ForEach(card => UnityEngine.Debug.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator Currently held card regardles of stats: {card}"));
+            CommitmentCards.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator SelectRandomCardWithStats called with player {player}");
+            player.data.currentCards.ForEach(card => CommitmentCards.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator Currently held card regardles of stats: {card}"));
             var cards = player.data.currentCards.Where(card => card.cardStats.Length > 0 && ModdingUtils.Utils.Cards.instance.PlayerIsAllowedCard(player, card)).ToList();
-            UnityEngine.Debug.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator SelectRandomCardWithStats Currently held card with stats: {cards}");
-            cards.ForEach(card => UnityEngine.Debug.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator card: {card}"));
+            CommitmentCards.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator SelectRandomCardWithStats Currently held card with stats: {cards}");
+            cards.ForEach(card => CommitmentCards.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator card: {card}"));
             
             if (cards == null)
             {
-                UnityEngine.Debug.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator cards with stats was null ");
+                CommitmentCards.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator cards with stats was null ");
                 return null;
             }
             if (cards.Count == 0)
             {
-                UnityEngine.Debug.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator cards with stats was empty ");
+                CommitmentCards.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator cards with stats was empty ");
                 return null;
             }
             var randomCard = cards[UnityEngine.Random.Range(0, cards.Count)];
-            UnityEngine.Debug.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator Random card selected: {randomCard}");
+            CommitmentCards.Log($"[{CommitmentCards.ModInitials}][MonoBehaviour] HandManipulator Random card selected: {randomCard}");
             return randomCard;
         }
     }
