2ab1d62c333bcc279fcce3a8ca51ee0d658ae581
pdcook <qphysicscook@gmail.com>
2021-11-04 01:28:50 -0400

    fixed persistance issue with Hose

---
+++ RoundsMod/Patches/PlayerPatchFullReset.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using HarmonyLib;
+using CommitmentCards.Scripts;
+using UnityEngine;
+
+namespace CommitmentCards.Patches
+{
+    // patch to reset card effects
+    [Serializable]
+    [HarmonyPatch(typeof(Player), "FullReset")]
+    class PlayerPatchFullReset
+    {
+        private static void Prefix(Player __instance)
+        {
+            if (__instance.GetComponent<ConstantFire>() != null) { UnityEngine.GameObject.Destroy(__instance.GetComponent<ConstantFire>()); }
+        }
+    }
+}
